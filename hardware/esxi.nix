557c9672961d7e0807f3886e3a2df19f781dc675
jerrita <je5r1ta@icloud.com>
2024-02-21 16:43:22 +0800

    fuck

--- hardware/esxi.nix
+++ hardware/esxi.nix
@@ -66,16 +66,15 @@ in {
         matchConfig.PermanentMACAddress = "1c:83:41:40:c1:01";
         linkConfig.Name = "wan";
     };
-    "10-vmnet" = {
-        matchConfig.PermanentMACAddress = "00:0c:29:85:39:89";
-        linkConfig.Name = "intern0";
-    };
+    # "10-vmnet" = {
+    #     matchConfig.PermanentMACAddress = "00:0c:29:85:39:89";
+    #     linkConfig.Name = "intern0";
+    # };
     "10-r8168" = {
         matchConfig.PermanentMACAddress = "1c:83:41:40:c1:00";
-        linkConfig.Name = "intern1";
+        linkConfig.Name = "lan";
     };
   };
-  networking.bridges.lan.interfaces = [ "intern0" "intern1" ];
 
   virtualisation.vmware.guest.enable = true;
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
