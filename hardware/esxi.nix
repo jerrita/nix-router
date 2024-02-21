d626fe6bbd29efe88abc85938885ee6e9cb62690
jerrita <je5r1ta@icloud.com>
2024-02-21 11:54:02 +0800

    sync

--- hardware/esxi.nix
+++ hardware/esxi.nix
@@ -56,8 +56,12 @@ in {
   };
 
   systemd.network.links = {
-    "10-wan" = {
-        matchConfig.PermanentMACAddress = "00:0c:29:85:39:93";
+    # "10-wan" = {  # UsbNetwork
+    #     matchConfig.PermanentMACAddress = "00:0c:29:85:39:93";
+    #     linkConfig.Name = "wan";
+    # };
+    "10-wan" = {   # Phy
+        matchConfig.PermanentMACAddress = "1c:83:41:40:c1:01";
         linkConfig.Name = "wan";
     };
     "10-lan" = {
