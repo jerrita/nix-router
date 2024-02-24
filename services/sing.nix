bb07f5d4940d937bb25645d017813243d6ce9359
jerrita <je5r1ta@icloud.com>
2024-02-24 21:33:40 +0800

    singbox: no route

--- services/sing.nix
+++ services/sing.nix
@@ -35,10 +35,10 @@ let
                 type = "tun";
                 inet4_address = "172.19.0.1/30";
                 gso = true;
-                auto_route = true;
-                inet4_route_exclude_address = [
-                    "192.168.5.0/24"
-                ];
+                # auto_route = true;
+                # inet4_route_exclude_address = [
+                #     "192.168.5.0/24"
+                # ];
             }
         ];
         route = {
@@ -72,13 +72,47 @@ in {
     systemd.services.sing-box = {
         postStart = ''
             sed -i 's/server=127.0.0.1#5353/server=127.0.0.1#5355/g' /etc/special.conf
-            sed -i 's/^conf-dir=\/etc\/dnsmasq.d/#conf-dir=\/etc\/dnsmasq.d/g' /etc/special.conf
+            # sed -i 's/^conf-dir=\/etc\/dnsmasq.d/#conf-dir=\/etc\/dnsmasq.d/g' /etc/special.conf
             systemctl restart dnsmasq
+            declare -a ncn_list=(
+                "198.18.0.0/15"
+                "8.8.8.8/32"
+                "1.1.1.1/32"
+                "91.108.4.0/22"
+                "91.108.8.0/22"
+                "91.108.12.0/22"
+                "91.108.16.0/22"
+                "91.108.20.0/22"
+                "91.108.56.0/22"
+                "91.108.192.0/22"
+                "149.154.160.0/20"
+                "185.76.151.0/24"
+            )
+            for cidr in "''${ncn_list[@]}"; do
+                ip route add $cidr via 172.19.0.2
+            done
         '';
         postStop = ''
             sed -i 's/server=127.0.0.1#5355/server=127.0.0.1#5353/g' /etc/special.conf
-            sed -i 's/^#conf-dir=\/etc\/dnsmasq.d/conf-dir=\/etc\/dnsmasq.d/g' /etc/special.conf
+            # sed -i 's/^#conf-dir=\/etc\/dnsmasq.d/conf-dir=\/etc\/dnsmasq.d/g' /etc/special.conf
             systemctl restart dnsmasq
+            declare -a ncn_list=(
+                "198.18.0.0/15"
+                "8.8.8.8/32"
+                "1.1.1.1/32"
+                "91.108.4.0/22"
+                "91.108.8.0/22"
+                "91.108.12.0/22"
+                "91.108.16.0/22"
+                "91.108.20.0/22"
+                "91.108.56.0/22"
+                "91.108.192.0/22"
+                "149.154.160.0/20"
+                "185.76.151.0/24"
+            )
+            for cidr in "''${ncn_list[@]}"; do
+                ip route delete $cidr via 172.19.0.2
+            done
         '';
     };
 }
