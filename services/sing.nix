8b537a2db270f4edbe73b79b773cc2e88d4176a6
jerrita <je5r1ta@icloud.com>
2024-02-25 20:28:30 +0800

    try fix

--- services/sing.nix
+++ services/sing.nix
@@ -34,6 +34,29 @@ let
                 type = "tun";
                 inet4_address = "172.19.0.1/30";
                 gso = true;
+                auto_route = true;
+                inet4_route_address = [
+                    "198.18.0.0/15"
+                    "8.8.8.8/32"
+                    "1.1.1.1/32"
+                    "91.108.4.0/22"
+                    "91.108.8.0/22"
+                    "91.108.12.0/22"
+                    "91.108.16.0/22"
+                    "91.108.20.0/22"
+                    "91.108.56.0/22"
+                    "91.108.192.0/22"
+                    "149.154.160.0/20"
+                    "185.76.151.0/24"
+                ];
+                inet6_route_address = [
+                    "fc00::/18"
+                    "2001:b28:f23d::/48"
+                    "2001:b28:f23f::/48"
+                    "2001:67c:4e8::/48"
+                    "2001:b28:f23c::/48"
+                    "2a0a:f280::/32"
+                ];
             }
         ];
         route = {
@@ -46,7 +69,7 @@ let
         experimental = {
             cache_file = {
                 enabled = true;
-                store_fakeip = false;
+                store_fakeip = true;
             };
             clash_api = {
                 external_controller = "192.168.5.1:9090";
@@ -63,44 +86,44 @@ in {
         settings = settings // extraSettings;
     };
     # 在运行一段时间后，inbounds 会堆积，导致内存占用过高，因此需要定时重启
-    services.cron.systemCronJobs = [
-        "*/30 * * * * root ${pkgs.systemd}/bin/systemctl restart sing-box"
-    ];
+    # services.cron.systemCronJobs = [
+    #     "*/30 * * * * root ${pkgs.systemd}/bin/systemctl restart sing-box"
+    # ];
     systemd.services.sing-box = {
         postStart = ''
             sed -i 's/server=127.0.0.1#5353/server=127.0.0.1#5355/g' /etc/special.conf
             # sed -i 's/^conf-dir=\/etc\/dnsmasq.d/#conf-dir=\/etc\/dnsmasq.d/g' /etc/special.conf
             systemctl restart dnsmasq
-            PATH=/run/current-system/sw/bin:$PATH
-            declare -a ncn_list=(
-                "198.18.0.0/15"
-                "8.8.8.8/32"
-                "1.1.1.1/32"
-                "91.108.4.0/22"
-                "91.108.8.0/22"
-                "91.108.12.0/22"
-                "91.108.16.0/22"
-                "91.108.20.0/22"
-                "91.108.56.0/22"
-                "91.108.192.0/22"
-                "149.154.160.0/20"
-                "185.76.151.0/24"
-            )
-            declare -a ncn_list6=(
-                "fc00::/18"
-                "2001:b28:f23d::/48"
-                "2001:b28:f23f::/48"
-                "2001:67c:4e8::/48"
-                "2001:b28:f23c::/48"
-                "2a0a:f280::/32"
-            )
-            while ! ip a show tun0 &> /dev/null; do sleep 1; done
-            for cidr in "''${ncn_list[@]}"; do
-                ip route add $cidr dev tun0
-            done
-            for cidr in "''${ncn_list6[@]}"; do
-                ip -6 route add $cidr dev tun0
-            done
+            # PATH=/run/current-system/sw/bin:$PATH
+            # declare -a ncn_list=(
+            #     "198.18.0.0/15"
+            #     "8.8.8.8/32"
+            #     "1.1.1.1/32"
+            #     "91.108.4.0/22"
+            #     "91.108.8.0/22"
+            #     "91.108.12.0/22"
+            #     "91.108.16.0/22"
+            #     "91.108.20.0/22"
+            #     "91.108.56.0/22"
+            #     "91.108.192.0/22"
+            #     "149.154.160.0/20"
+            #     "185.76.151.0/24"
+            # )
+            # declare -a ncn_list6=(
+            #     "fc00::/18"
+            #     "2001:b28:f23d::/48"
+            #     "2001:b28:f23f::/48"
+            #     "2001:67c:4e8::/48"
+            #     "2001:b28:f23c::/48"
+            #     "2a0a:f280::/32"
+            # )
+            # while ! ip a show tun0 &> /dev/null; do sleep 1; done
+            # for cidr in "''${ncn_list[@]}"; do
+            #     ip route add $cidr dev tun0
+            # done
+            # for cidr in "''${ncn_list6[@]}"; do
+            #     ip -6 route add $cidr dev tun0
+            # done
         '';
         postStop = ''
             sed -i 's/server=127.0.0.1#5355/server=127.0.0.1#5353/g' /etc/special.conf
