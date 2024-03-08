8b46c18a1be45b3a9cba5c33fa7a12d9624a3165
jerrita <je5r1ta@icloud.com>
2024-03-08 20:14:31 +0800

    vscode: use nix-ld

--- services/vscode.nix
+++ services/vscode.nix
@@ -1,11 +1,4 @@
 { config, pkgs, ... }:
 {
-    imports = [
-        (fetchTarball {
-            url = "https://github.com/nix-community/nixos-vscode-server/tarball/master";
-            sha256 = "1mrc6a1qjixaqkv1zqphgnjjcz9jpsdfs1vq45l1pszs9lbiqfvd";
-        })
-    ];
-
-    services.vscode-server.enable = true;
+    programs.nix-ld.enable = true;
 }
