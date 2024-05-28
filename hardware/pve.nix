74bdd25c8967cbe7f2458a9420b0094f3b0cae56
jerrita <je5r1ta@icloud.com>
2024-05-28 13:11:00 +0800

    fix: pve

--- hardware/pve.nix
+++ hardware/pve.nix
@@ -19,6 +19,10 @@ in
       (modulesPath + "/profiles/qemu-guest.nix")
     ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+
   boot.initrd.availableKernelModules = [ "uhci_hcd" "ehci_pci" "ahci" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
