1206f7d55fc5d1fac89373a3bac3a8c35f4ed5c3
jerrita <je5r1ta@icloud.com>
2024-07-09 17:06:31 +0800

    fix: clash

--- services/clash.nix
+++ services/clash.nix
@@ -27,19 +27,9 @@
             Type = "simple";
             User = "clash";
             Group = "clash";
-            # ExecStartPre = "+/etc/clash/scripts/clash-pre";
-            ExecStartPre = "+${pkgs.writeScript "preStart" ''#!/usr/bin/env bash
-                mkdir -p /etc/clash
-                chown -R clash:clash /etc/clash
-                sed -i 's/server=127.0.0.1#5353/server=127.0.0.1#5355/g' /etc/dnsmasq.conf
-                systemctl restart dnsmasq
-            ''}";
+            ExecStartPre = "+/etc/scripts/clash-pre";
             ExecStart = "${pkgs.mihomo}/bin/mihomo -d /etc/clash";
-            ExecStopPost = "+${pkgs.writeScript "postStop" ''#!/usr/bin/env bash
-                sed -i 's/server=127.0.0.1#5355/server=127.0.0.1#5353/g' /etc/dnsmasq.conf
-                systemctl restart dnsmasq
-            ''}";
-            # ExecStop = "+/etc/clash/scripts/clash-post";
+            ExecStop = "+/etc/scripts/clash-post";
             Restart = "on-failure";
             CapabilityBoundingSet="CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW";
             AmbientCapabilities="CAP_NET_ADMIN CAP_NET_BIND_SERVICE CAP_NET_RAW";
