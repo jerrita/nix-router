8bd7940e76d355327e42edb177b001a8667287d3
jerrita <je5r1ta@icloud.com>
2024-07-16 12:29:34 +0800

    tryfix

--- services/wireguard.nix
+++ services/wireguard.nix
@@ -8,23 +8,23 @@
         path = with pkgs; [ wireguard-tools ];
         serviceConfig = {
             Type = "oneshot";
-            script = ''
-                ${pkgs.kmod}/bin/modprobe wireguard
-                mkdir -p /etc/wireguard
-                for conf in /etc/wireguard/*.conf; do
-                    interface=$(basename $conf .conf)
-                    wg-quick up $interface
-                done
-            '';
-            preStop = ''
-                for conf in /etc/wireguard/*.conf; do
-                    interface=$(basename $conf .conf)
-                    wg-quick down $interface
-                done
-            '';
             PrivateTmp = true;
             RemainAfterExit = true;
         };
+        script = ''
+            ${pkgs.kmod}/bin/modprobe wireguard
+            mkdir -p /etc/wireguard
+            for conf in /etc/wireguard/*.conf; do
+                interface=$(basename $conf .conf)
+                wg-quick up $interface
+            done
+        '';
+        preStop = ''
+            for conf in /etc/wireguard/*.conf; do
+                interface=$(basename $conf .conf)
+                wg-quick down $interface
+            done
+        '';
     };
     environment.systemPackages = [ pkgs.wireguard-tools ];
 }
