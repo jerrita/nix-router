e881e95ab7449ded3d017e5f44ebbdc3ee967eaf
jerrita <je5r1ta@icloud.com>
2024-07-19 08:11:44 +0000

    use: cached kernel

--- flake.nix
+++ flake.nix
@@ -1,67 +1,49 @@
 {
-    description = "Jerrita's Router Flake";
+  description = "Jerrita's Router Flake";
 
-    nixConfig = {
-        experimental-features = [ "nix-command" "flakes" ];
-        substituters = [
-            "https://mirrors.ustc.edu.cn/nix-channels/store"
-            "https://cache.nixos.org"
-        ];
-        allowUnfree = true;
-    };
+  nixConfig = {
+    experimental-features = [ "nix-command" "flakes" ];
+    substituters = [
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://cache.nixos.org"
+    ];
+    extra-substituters = [
+      "https://jerrita.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "jerrita.cachix.org-1:nuNrOWU7/dWbGwwE5bwUfvycsiPHb5NnD/aIZIcaPDI="
+    ];
+  };
 
-    inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        scripts = {
-            url = "github:jerrita/scripts";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-        # utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
-    };
-
-    outputs = { self, nixpkgs, scripts } @ inputs: rec {
-        nixosConfigurations.r2s = nixpkgs.lib.nixosSystem {
-            system = "aarch64-linux";
-            specialArgs = {inherit nixpkgs;};
-            modules = [
-                ./hardware/r2s.nix
-                ./router
-                scripts.nixosModules.ddns
-            ];
-        };
-        images.r2s = nixosConfigurations.r2s.config.system.build.sdImage;
-
-        nixosConfigurations.esxi = nixpkgs.lib.nixosSystem {
-            system = "x86_64-linux";
-            specialArgs = {inherit nixpkgs;};
-            modules = [
-                ./hardware/esxi.nix
-                ./router
-                scripts.nixosModules.ddns
-            ];
-        };
-    };
-
-    # outputs = { self, nixpkgs, scripts, utils, ... } @ inputs:
-    #     utils.lib.mkFlake 
-    # {
-    #     inherit self inputs;
-
-    #     channelsConfig.allowUnfree = true;
-    #     hostDefaults.modules = [ 
-    #         ./router
-    #         scripts.nixosModules.ddns
-    #     ];
-
-    #     channels.patched.input = nixpkgs;
-    #     # channels.patched.patches = [ ];
-
-    #     hosts.esxi = {
-    #         system = "x86_64-linux";
-    #         channelName = "patched";
-    #         modules = [
-    #             ./hardware/esxi.nix
-    #         ];
-    #     };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
+    # scripts = {
+    #   url = "github:jerrita/scripts";
+    #   inputs.nixpkgs.follows = "nixpkgs";
     # };
+    jerrita.url = "github:jerrita/nur";
+    # utils.url = "github:gytis-ivaskevicius/flake-utils-plus";
+  };
+
+  outputs = { self, nixpkgs, jerrita } @ inputs: rec {
+    nixosConfigurations.r2s = nixpkgs.lib.nixosSystem {
+      system = "aarch64-linux";
+      specialArgs = { inherit nixpkgs; inherit jerrita; };
+      modules = [
+        ./hardware/r2s.nix
+        ./router
+      ];
+    };
+    images.r2s = nixosConfigurations.r2s.config.system.build.sdImage;
+
+    nixosConfigurations.esxi = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit nixpkgs; };
+      modules = [
+        ./hardware/esxi.nix
+        ./router
+        # scripts.nixosModules.ddns
+      ];
+    };
+  };
 }
--- hardware/r2s.nix
+++ hardware/r2s.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, lib, linux_r2s, ... }:
 let
   tuningScript = pkgs.writeScript "tuning" ''
     #!/usr/bin/env bash
@@ -15,91 +15,15 @@ let
     ethtool -G lan rx 1024
     ethtool -G wan rx 4096
   '';
-  kernel_ccache = pkgs.linuxPackages_latest.kernel.override {
-    stdenv = pkgs.ccacheStdenv;
-    buildPackages = pkgs.buildPackages // {
-      stdenv = pkgs.ccacheStdenv;
-    };
-  };
-in {
+
+in
+{
   imports = [
     # ../modules/extlinux.nix
     ../modules/r2s-image.nix
   ];
 
-  boot.kernelPackages = pkgs.linuxPackagesFor kernel_ccache;
-  boot.kernelPatches = [
-    { name = "enable-bbr3"; patch = ../patches/bbr3.patch; }
-    {
-      name = "hack";
-      patch = null;
-      # build reduced kernel to reduce compilation time
-      extraStructuredConfig =
-        let
-          no = lib.mkForce lib.kernel.no;
-        in
-        {
-          EFI_STUB = no;
-          EFI = no;
-          DRM_ACCEL = no;
-          # ACCESSIBILITY = no;
-          ACPI = no;
-          ATA = no;
-          ATM_DRIVERS = no;
-          AUXDISPLAY = no;
-          # BCMA = no;
-          COMEDI = no;
-          CXL_BUS = no;
-          # DAX = no;
-          DCA = no;
-          DPLL = no;
-          EISA = no;
-          EXTCON = no;
-          FIREWIRE = no;
-          FPGA = no;
-          FSI = no;
-          DRM = no;
-          FB = no;
-          HTE = no;
-          HYPERV = no;
-          IIO = no;
-          INFINIBAND = no;
-          INTERCONNECT = no;
-          IOMMU_SUPPORT = no;
-          IPACK_BUS = no;
-          ISDN = no;
-          MACINTOSH_DRIVERS = no;
-          MCB = no;
-          MEDIA_SUPPORT = no;
-          MEMSTICK = no;
-          FUSION = no;
-          MOST = no;
-          NVME_KEYRING = no;
-          NVME_AUTH = no;
-          NVME_CORE = no;
-          NVME_TARGET = no;
-          # DTC = no;
-          OF_ALL_DTBS = no;
-          PCI = no;
-          PCCARD = no;
-          PECI = no;
-          RAPIDIO = no;
-          RAS = no;
-          REMOTEPROC = no;
-          RPMSG = no;
-          SCSI = no;
-          STAGING = no;
-          USB4 = no;
-          VDPA = no;
-          XEN = no;
-          ZORRO_NAMES = no;
-          SOUND = no;
-          SND = no;
-          VIDEO = no;
-          HDMI = no;
-        };
-    }
-  ];
+  boot.kernelPackages = linux_r2s.kernel;
 
   fileSystems = {
     "/boot" = {
@@ -134,43 +58,22 @@ in {
     "net.core.dev_weight" = 600;
   };
 
-  nixpkgs.overlays = [(final: super: {
-    zfs = super.zfs.overrideAttrs(_: {
-      meta.platforms = [];
-    });
-    ccacheWrapper = super.ccacheWrapper.override {
-        extraConfig = ''
-          export CCACHE_COMPRESS=1
-          export CCACHE_DIR="${config.programs.ccache.cacheDir}"
-          export CCACHE_UMASK=007
-          if [ ! -d "$CCACHE_DIR" ]; then
-            echo "====="
-            echo "Directory '$CCACHE_DIR' does not exist"
-            echo "Please create it with:"
-            echo "  sudo mkdir -m0770 '$CCACHE_DIR'"
-            echo "  sudo chown root:nixbld '$CCACHE_DIR'"
-            echo "====="
-            exit 1
-          fi
-          if [ ! -w "$CCACHE_DIR" ]; then
-            echo "====="
-            echo "Directory '$CCACHE_DIR' is not accessible for user $(whoami)"
-            echo "Please verify its access permissions"
-            echo "====="
-            exit 1
-          fi
-        '';
-    };
-  })];
+  nixpkgs.overlays = [
+    (final: super: {
+      zfs = super.zfs.overrideAttrs (_: {
+        meta.platforms = [ ];
+      });
+    })
+  ];
 
   systemd.services.tuning = {
-      wantedBy = [ "multi-user.target" ];
-      after = [ "network.target" ];
-      path = [ pkgs.bash pkgs.ethtool ];
-      serviceConfig = {
-          Type = "oneshot";
-          ExecStart = "${tuningScript}";
-      };
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+    path = [ pkgs.bash pkgs.ethtool ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${tuningScript}";
+    };
   };
 
   hardware.deviceTree.name = "rockchip/rk3328-nanopi-r2s.dtb";
@@ -219,16 +122,16 @@ in {
   powerManagement.cpuFreqGovernor = "schedutil";
 
   systemd.network.links = {
-      "10-wan" = {
-          # ff600000.usb/xhci-hcd.0.auto/usb5/5-1/5-1:1.0/net/enu1
-          # matchConfig.Path = "platform-ff600000.usb";
-          matchConfig.Driver = "r8152";
-          linkConfig.Name = "wan";
-      };
-      "10-lan" = {
-          matchConfig.Path = "platform-ff540000.ethernet";
-          linkConfig.Name = "lan";
-      };
+    "10-wan" = {
+      # ff600000.usb/xhci-hcd.0.auto/usb5/5-1/5-1:1.0/net/enu1
+      # matchConfig.Path = "platform-ff600000.usb";
+      matchConfig.Driver = "r8152";
+      linkConfig.Name = "wan";
+    };
+    "10-lan" = {
+      matchConfig.Path = "platform-ff540000.ethernet";
+      linkConfig.Name = "lan";
+    };
   };
 
   services.lvm.enable = false;
@@ -276,3 +179,4 @@ in {
     '';
   };
 }
+
